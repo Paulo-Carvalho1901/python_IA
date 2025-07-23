5f2bc2d5df1e2e006b35d337f5a6299e697765d6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-23 17:56:14 -0300

    aprendendo sobre variaveis, conceitos fundamentais

---
+++ 01_fundamentos/01_variavel.py
@@ -0,0 +1,23 @@
+# variables
+
+'''
+Nome de variavel não aceita:
+
+    - espaços
+    - começar com numeros
+    - não utilizar -
+    - não inicar letra maiuscula
+'''
+
+'''
+Tipos de dados:
+
+texto - str
+    - numero inteiro - int
+    - numero decimaç - float
+    - verdadeiro ou falso (Valores lógicos) - bool
+'''
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print('Olá', nome, 'Você tem', idade, 'anos de idade.')
