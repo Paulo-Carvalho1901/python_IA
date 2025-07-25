e2ab34f9f587b9b5b0d00024c336f53dc317fa8b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-25 14:42:18 -0300

    fazendo a coerção de tipo direto no input

---
+++ 01_fundamentos/03_operadores_aritimericos.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+print(f'Olá {nome} daqui 5 anos você tera {idade + 5} anos de idade.')
+
+
+print(type(nome))
+print(type(idade))
