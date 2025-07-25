dc3df28cfee436898a6c2123f8ae37113d77ae78
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-25 14:49:47 -0300

    operadores aritmeticosa em python

--- 01_fundamentos/03_operadores_aritimericos.py
+++ 01_fundamentos/03_operadores_aritimericos.py
@@ -1,8 +1,32 @@
-nome = input('Digite seu nome: ')
-idade = int(input('Digite sua idade: '))
+# nome = input('Digite seu nome: ')
+# idade = int(input('Digite sua idade: '))
 
-print(f'Olá {nome} daqui 5 anos você tera {idade + 5} anos de idade.')
+# print(f'Olá {nome} daqui 5 anos você tera {idade + 5} anos de idade.')
 
 
-print(type(nome))
-print(type(idade))
+# print(type(nome))
+# print(type(idade))
+
+numero1 = 10
+numero2 = 3
+
+# soma
+print(numero1 + numero2)
+
+# subtração
+print(numero1 - numero2)
+
+# multiplicação
+print(numero1 * numero2)
+
+# divisão
+print(numero1 / numero2)
+
+# divisão inteira
+print(numero1 // numero2)
+
+# resto divisão
+print(numero1 % numero2)
+
+# potenciação
+print(numero1 ** numero2)
