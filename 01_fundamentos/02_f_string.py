d4fcecf75c336ad5ba04beb9ac145b17f27d45f4
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-25 14:32:08 -0300

    finalizando f string

--- 01_fundamentos/02_f_string.py
+++ 01_fundamentos/02_f_string.py
@@ -1,9 +1,13 @@
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
-fruta_favorita = input('Qual sua fruta favorita? ')
+
+idade_int = int(idade)
+# fruta_favorita = input('Qual sua fruta favorita? ')
 
 # Sem f-string
-print('Olá', nome, 'Você tem', idade, 'anos de idade.')
+# print('Olá', nome, 'Você tem', idade, 'anos de idade.')
 
 # f-string
-print(f'Olá, {nome} você tem {idade} anos de idade e sua fruta favorita é {fruta_favorita}.')
+# print(f'Olá, {nome} você tem {idade} anos de idade e sua fruta favorita é {fruta_favorita}.')
+
+print(f'Olá {nome} daqui 5 anos você tera {idade_int + 5} de idade.')
