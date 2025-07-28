faf2de702a533daeafe85ddb432aa240d2000ecd
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-28 15:18:18 -0300

    introdução aos aperadores relacionais

---
+++ 01_fundamentos/06_operadores_relacionais.py
@@ -0,0 +1,27 @@
+"""
+Operadores relacionais
+são utilizados para comparação de valores
+e retorna um valor True ou False
+"""
+
+
+num1 = 10
+num2 = 12
+
+# == (igual)
+print(num1 == num2)
+
+# != (diferente)
+print(num1 != num2)
+
+# > (naior que)
+print(num1 > num2)
+
+# < (menor que)
+print(num1 < num2)
+
+# >= (naior ou igua)
+print(num1 >= num2)
+
+# <= (menor ou igual)
+print(num1 <= num2)
