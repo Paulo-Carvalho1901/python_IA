6e239bd6336f3e924d1ed968b6684a847a6c1888
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-28 16:30:54 -0300

    introducação a estrutura condicionais if else

---
+++ 01_fundamentos/09_condicoes_if_elif_else.py
@@ -0,0 +1,14 @@
+"""
+Condições
+
+if, else
+se, se não
+
+"""
+
+numero = int(input('Digit um número: '))
+
+if numero == 10:
+    print('Você Acertou o número.')
+else:
+    print('Você não acertou o número.')
