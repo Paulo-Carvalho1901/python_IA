672917887a466b2f09aea6badec3428858d43f95
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-28 15:58:00 -0300

    add exemplo com operador logico and

--- 01_fundamentos/07_exercicio_operadores_relacional.py
+++ 01_fundamentos/07_exercicio_operadores_relacional.py
@@ -1,6 +1,7 @@
 # Igual ou maior que 18 anos de idade é adulto
 
 idade = int(input('Digite sua idade: '))
-verificador = idade >= 18
+carteira = False
+verificador = idade >= 18 and carteira
 
-print(f'Você é maior de idade? {verificador}.')
+print(f'Posso dirigir? {verificador}.')
