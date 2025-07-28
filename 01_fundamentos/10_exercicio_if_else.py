15c0b29f566f5f6e74574d76f77a466893dc9c14
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-28 16:36:53 -0300

    exercicio maior ou menor de idade

---
+++ 01_fundamentos/10_exercicio_if_else.py
@@ -0,0 +1,9 @@
+# Verificar se é maior de idade
+
+idade = int(input('Digite sua idade: '))
+
+if idade >= 18:
+    print('Usuário é maioor de idade.')
+else:
+    print('Usuário é menor de idade.')
+    
